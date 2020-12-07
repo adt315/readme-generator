8a0651f8837a0d6d8d7d71fa39ccbd3f485396cd
adt315 <angela_dyan@hotmail.com>
2020-12-06 19:04:31 -0500

    add answers

--- index.js
+++ index.js
@@ -1,46 +1,18 @@
 
-// array of questions for user
-const questions = [
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
-
-/////////////START///////////////////////////
-
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-const axios = require("axios");
-const generateMarkdown = require("./util/generateMarkdown.js");
+// const axios = require("axios");
+// const generateMarkdown = require("./util/generateMarkdown.js");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
+// // array of questions for user
+// const questions = [
+// ];
+
 function promptUser() {
   return inquirer.prompt([
-
-    // The generated README includes the following sections:
-    // Title
-    // 1 badge that's specific to the repository.
-    // Description
-    // Table of Contents - WHEN I click on the links in the Table of Contents THEN I am taken to the corresponding section of the README
-    // Installation
-    // Usage
-    // License
-    // Contributing
-    // Tests
-    // Questions
-
-
     // project title
     {
       type: "input",
@@ -48,20 +20,28 @@ function promptUser() {
       message: "What is the title of your project?"
     },
     // WHEN I choose a license for my application from a list of options THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-    // {
-     
-
-    // },
+    {
+      type: "input",
+      name: "license",
+      message: "license"
+    },
     // The generated README includes 1 badge that's specific to the repository.
-    // {
-    
-
-    // },
+    {
+      type: "input",
+      name: "badge",
+      message: "badge"
+    },
     // description
     {
       type: "input",
       name: "description",
-      message: "Please type a brief description of your project."
+      message: "Please give a brief description of your project."
+    },
+    // table of contents
+    {
+      type: "input",
+      name: "contents",
+      message: "Please list your Table of Contents."
     },
     // installation instructions
     {
@@ -73,13 +53,13 @@ function promptUser() {
     {
       type: "input",
       name: "usage",
-      message: "Please type usage information about your project."
+      message: "What are the usage guidelines for your project?"
     },
     // contribution guidelines
     {
       type: "input",
       name: "contribution",
-      message: "What are the guidelines for contribution to this project?"
+      message: "Who contributed to this project? What are the guidelines for contributing to this project?"
     },
     // test instructions
     {
@@ -87,6 +67,7 @@ function promptUser() {
       name: "test",
       message: "Please type instructions for how to test your project."
     },
+    //QUESTIONS
     // WHEN I enter my GitHub username THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
     {
       type: "input",
@@ -98,56 +79,91 @@ function promptUser() {
     type: "input",
     name: "email",
     message: "What is your email address?"
-  },
+  }
   ]);
 }
 
-function generateHTML(answers) {
-  return `
+ function generateMarkdown(answers) {
+   return `
+    # ${answers.title}
 
-    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
-    <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
-}
+    # ${answers.license}
 
-////Regular Ending///////
+    # ${answers.badge}
+
+    # ${answers.description}
+
+    # ${answers.contents}
+
+    # ${answers.installation}
+
+    # ${answers.usage}
+
+    # ${answers.contribution}
+
+    # ${answers.test}
+
+    # ${answers.GitHub}
+
+    # ${answers.email}
+`;
+ }
+
+// ////Regular Ending///////
 promptUser()
   .then(function(answers) {
-    const html = generateHTML(answers);
+    const html = generateMarkdown(answers);
 
-    return writeFileAsync("index.html", html);
+    return writeFileAsync("generatedREADME", html);
   })
   .then(function() {
-    console.log("Successfully wrote to index.html");
+    console.log("README is created");
   })
   .catch(function(err) {
     console.log(err);
   });
 
+  
+// //////BONUS Ending/////////////
+// async function init() {
+//     console.log("hi")
+//     try {
+//       const answers = await promptUser();
+  
+//       const html = generateHTML(answers);
+  
+//       await writeFileAsync("index.html", html);
+  
+//       console.log("README is created");
+//     } catch(err) {
+//       console.log(err);
+//     }
+//   } 
+//   init();
 
-//////BONUS Ending/////////////
-async function init() {
-    console.log("hi")
-    try {
-      const answers = await promptUser();
-  
-      const html = generateHTML(answers);
-  
-      await writeFileAsync("index.html", html);
-  
-      console.log("Successfully wrote to index.html");
-    } catch(err) {
-      console.log(err);
-    }
-  }
-  
-  init();
+
+
+// // function to write README file - generateMarkdown function 
+// function writeToFile(fileName, data) {
+// }
+
+// // function to initialize program
+// function init() {
+//   inquirer.prompt(questions)
+//   .then(answers => {
+//     console.log(`Answers -` answers),
+//   });
+// }
+
+// // function call to initialize program
+// init();
+
+
+
+
+
+
+
+
+
   
